bbe140a62ce803cc6456b0a0f926d69324737aba
Fabian Holzschuster <e12025338@student.tuwien.ac.at>
2023-06-13 12:15:12 +0200

    removed commented out code

--- ECG_Solution/src/Application.cpp
+++ ECG_Solution/src/Application.cpp
@@ -190,57 +190,6 @@ int main(int argc, char** argv) {
 	// setup for bloom
 	setupBloomBuffers();
 
-	/*
-	{
-		glGenFramebuffers(1, &hdrFrameBuffer);
-		glBindFramebuffer(GL_FRAMEBUFFER, hdrFrameBuffer);
-		// 2 floating point color buffers, 1 for normal rendering, 2 for brightness thresholds
-		glGenTextures(2, colorBuffers);
-		for (unsigned int i = 0; i < 2; i++) {
-			glBindTexture(GL_TEXTURE_2D, colorBuffers[i]);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-			// attach texture to corresponding framebuffer
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
-		}
-		// create and attach depthBuffer (renderBuffer)
-		glGenRenderbuffers(1, &renderBufferDepth);
-		glBindRenderbuffer(GL_RENDERBUFFER, renderBufferDepth);
-		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, screenWidth, screenHeight);
-		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, renderBufferDepth);
-		// tell openGL which color attachments to use for rendering
-		attachments[0] = GL_COLOR_ATTACHMENT0;
-		attachments[1] = GL_COLOR_ATTACHMENT1;
-		glDrawBuffers(2, attachments);
-		// check framebuffer for completeness
-		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-			std::cout << "Framebuffer (depth) not complete!" << std::endl;
-		}
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-		// ping-pong framebuffer for blurring (bloom effect around light)
-		glGenFramebuffers(2, pingPongFrameBuffer);
-		glGenTextures(2, pingPongColorBuffers);
-		for (unsigned int i = 0; i < 2; i++) {
-			glBindFramebuffer(GL_FRAMEBUFFER, pingPongFrameBuffer[i]);
-			glBindTexture(GL_TEXTURE_2D, pingPongColorBuffers[i]);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingPongColorBuffers[i], 0);
-			// also check if framebuffers are complete (no need for depth buffer)
-			if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-				std::cout << "Framebuffer (blur) not complete!" << std::endl;
-			}
-		}
-	}
-	*/
-
 	bloomBlurShader = MyAssetManager::loadShader("bloomBlur.vert", "bloomBlur.frag", "bloomBlur");
 	bloomBlurShader.use();
 	bloomBlurShader.setInt("imageToBlur", 0);
@@ -577,7 +526,6 @@ int main(int argc, char** argv) {
 	scene->addObject(&leftWallCorridorSecondFifth);
 	scene->addObject(&groundCorridorSecondFifth);
 	
-
 	// Init Zombies
 	Zombie zombieOne{ &animationShader, gPhysics };
 	scene->addObject(&zombieOne, true);
@@ -806,6 +754,7 @@ int main(int argc, char** argv) {
 		}*/
 
 		renderHUD(textRenderer, textShader);
+
 		if (enableGUIHUD) {
 			guiRenderer.renderGUI(guiShader, score, maxScore, bullets, maxBullets, player->getHealth(), player->maxHealth);
 		}
